b5ae0120e2b31d338414b7957b1852394917a455
Galina4000 <galinka.bujanova@ya.ru>
2026-05-01 12:33:22 +0400

    filmorate 4

--- src/main/java/storage/film/FilmStorage.java
+++ src/main/java/storage/film/FilmStorage.java
@@ -5,9 +5,13 @@ import ru.yandex.practicum.filmorate.model.Film;
 import java.util.List;
 
 public interface FilmStorage {
+
     Film create(Film film);
+
     Film update(Film film);
+
     Film findById(Long id);
+
     List<Film> findAll();
 }
 
